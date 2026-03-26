88543de31c3479d82395c001ad8a81187e923219
tadipatri pradeep kumar reddy <129882259+pradeep7117@users.noreply.github.com>
2026-03-26 16:56:58 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import StorecontextProvider from './cONTEXT/Storecontext.jsx';
+import StoreContext from "./context/StoreContext.jsx";
 
 
 
